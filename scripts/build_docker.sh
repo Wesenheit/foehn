4f8ca21c3f29c91682ce8048792ae760480f3730
Mateusz Kapusta <mr.kapusta@student.uw.edu.pl>
2026-06-14 13:12:27 +0200

    add docker layer

---
+++ scripts/build_docker.sh
@@ -0,0 +1,18 @@
+docker buildx build \
+  --platform linux/arm64 \
+  --build-arg BASE_IMAGE=quay.io/pypa/manylinux_2_28_aarch64 \
+  --load \
+  -f Dockerfile.pmix \
+  -t ghcr.io/wesenheit/manylinux-pmix:5.0.9-aarch64 \
+  .
+
+docker buildx build \
+  --platform linux/amd64 \
+  --build-arg BASE_IMAGE=quay.io/pypa/manylinux_2_28_x86_64 \
+  --load \
+  -f Dockerfile.pmix \
+  -t ghcr.io/wesenheit/manylinux-pmix:5.0.9-x86_64 \
+  .
+
+docker push ghcr.io/wesenheit/manylinux-pmix:5.0.9-aarch64
+docker push ghcr.io/wesenheit/manylinux-pmix:5.0.9-x86_64
