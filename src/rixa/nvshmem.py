ddaf8f890a6d05909a05ba04065d6dd84b422be2
Mateusz Kapusta <mr.kapusta@student.uw.edu.pl>
2026-07-05 11:59:31 +0200

    move nvshmem to broadcast

--- src/rixa/nvshmem.py
+++ src/rixa/nvshmem.py
@@ -10,16 +10,7 @@ def init(dev: Device, store: PMIxStore, timeout: int = 30) -> None:
     rank = store.get_rank()
     uid = nvshmem.get_unique_id(empty=(rank != 0))
     uid_bytes = uid._data.view(np.uint8).tobytes()
-    if rank == 0:
-        store.set("root_uid", uid_bytes)
-
-    store.wait(
-        [
-            "root_uid",
-        ]
-    )
-
-    uid_bytes = store.get("root_uid")
+    uid_bytes = store.broadcastset("uid", uid_bytes, root=0)
     uid._data.view(np.uint8)[:] = np.frombuffer(uid_bytes, dtype=np.uint8)
 
     nvshmem.init(
